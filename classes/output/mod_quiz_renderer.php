81e08a0edf32167af228c9c07204d657c806cf06
Manisha Dayal <manisha.dayal@vidyamantra.com>
2021-05-25 13:23:06 +0530

    Issues resolved

--- classes/output/mod_quiz_renderer.php
+++ classes/output/mod_quiz_renderer.php
@@ -26,7 +26,7 @@ class mod_quiz_renderer extends \mod_quiz_renderer {
         if ($attemptobj->get_access_manager(time())->attempt_must_be_in_popup()) {
             $this->page->requires->js_init_call('M.mod_quiz.secure_window.init_close_button',
                     array($url), false, quiz_get_js_module());
-            return html_writer::empty_tag('input', array('type' => 'button',
+                    return  html_writer::empty_tag('input', array('type' => 'button',
                     'value' => get_string('finishreview', 'quiz'),
                     'id' => 'secureclosebutton',
                     'class' => 'mod_quiz-next-nav btn btn-primary'));
@@ -37,5 +37,44 @@ class mod_quiz_renderer extends \mod_quiz_renderer {
                     array('class' => 'mod_quiz-next-nav'));
             return $links;
         }
-    }	
+    }
+
+/**
+     * Outputs the navigation block panel
+     *
+     * @param quiz_nav_panel_base $panel instance of quiz_nav_panel_base
+     */
+    public function navigation_panel(\quiz_nav_panel_base $panel) {
+        $output = '';
+        $attemptid = required_param('attempt', PARAM_INT);
+        $printviewurl = new \moodle_url('/report/printquiz/index.php', array('attempt' => $attemptid));
+
+        $userpicture = $panel->user_picture();
+        if ($userpicture) {
+            $fullname = fullname($userpicture->user);
+            if ($userpicture->size === true) {
+                $fullname = html_writer::div($fullname);
+            }
+            $output .= html_writer::tag('div', $this->render($userpicture) . $fullname,
+                    array('id' => 'user-picture', 'class' => 'clearfix'));
+        }
+        $output .= $panel->render_before_button_bits($this);
+        $bcc = $panel->get_button_container_class();
+
+        $output .= html_writer::start_tag('div', array('class' => "qn_buttons clearfix $bcc"));
+        foreach ($panel->get_question_buttons() as $button) {
+            $output .= $this->render($button);
+        }
+        $output .= html_writer::end_tag('div');
+        
+        $output .= html_writer::tag('div', $panel->render_end_bits($this),
+                array('class' => 'othernav'));
+        $output .= html_writer::link($printviewurl, get_string('printquiz', 'theme_maker'),
+        array('class' => 'mod_quiz-next-nav'));
+        $this->page->requires->js_init_call('M.mod_quiz.nav.init', null, false,
+                quiz_get_js_module());
+
+        return $output;
+    }
+
 }
--- version.php
+++ version.php
@@ -16,7 +16,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021042800;
+$plugin->version   = 2021042806;
 $plugin->release  = 'Moodle 3.9 Maker v6.0';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2020060900; 
